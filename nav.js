ec8002eb4d54d13e4f3b3b724807e4cef68fbf34
Sebastian <s.albertogamarro5@gmail.com>
2022-02-27 21:46:17 -0700

    saved changes

---
+++ nav.js
@@ -0,0 +1,6 @@
+const hamburger = document.getElementById('hamburger-two')
+const nav = document.getElementById('navtwo-ol')
+
+hamburger.addEventListener('click', () =>{
+    nav.classList.toggle('show')
+})
